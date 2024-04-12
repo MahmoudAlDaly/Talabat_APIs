137e898e7377498be3ddf1aaaac22a646bdc5865
mahmoudaldaly <mahmo@DESKTOP-PVTMUIB>
2024-04-12 21:23:00 +0200

    Part 07 Onion Architecture Part 08 Product Module - Entities Part 09 DbContext, ProductConfigurations, Migrations

---
+++ Talabat_Core/Entities/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Talabat.Core.Entities
+{
+	public class Product : BaseEntity
+	{
+        public int Name { get; set; }
+		public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string PictureUrl { get; set; }
+
+
+        public ProductBrand Brand { get; set; }
+        public int BrandID { get; set; }
+
+
+        public ProductCategory Category { get; set; }
+        public int CategoryID { get; set;}
+
+    }
+}
