42ecd694fb33ad40350902c11987cead68242214
mahmoudaldaly <mahmo@DESKTOP-PVTMUIB>
2024-04-16 22:11:54 +0200

    Part 01 Specification Design Pattern

--- Talabat_Repository/Data/StoreContextSeed.cs
+++ Talabat_Repository/Data/StoreContextSeed.cs
@@ -12,49 +12,49 @@ namespace Talabat.Repository.Data
 	{
 		public async static Task SeedAsync(StoreContext context)
 		{
-			//if (context.ProductBrands.Count() == 0)
-			//{
-			//	var brandsdata = File.ReadAllText("../Talabat_Repository/Data/DataSeed/brands.json");
+			if (context.ProductBrands.Count() == 0)
+			{
+				var brandsdata = File.ReadAllText("../Talabat_Repository/Data/DataSeed/brands.json");
 
-			//	var brands = JsonSerializer.Deserialize<List<ProductBrand>>(brandsdata);
+				var brands = JsonSerializer.Deserialize<List<ProductBrand>>(brandsdata);
 
-			//	if (brands?.Count > 0)
-			//	{
-			//		brands = brands.Select(b => new ProductBrand()
-			//		{
-			//			Name = b.Name,
-			//		}).ToList();
+				if (brands?.Count > 0)
+				{
+					brands = brands.Select(b => new ProductBrand()
+					{
+						Name = b.Name,
+					}).ToList();
 
-			//		foreach (var item in brands)
-			//		{
-			//			context.Set<ProductBrand>().Add(item);
-			//		}
+					foreach (var item in brands)
+					{
+						context.Set<ProductBrand>().Add(item);
+					}
 
-			//		await context.SaveChangesAsync();
-			//	}
-			//}
+					await context.SaveChangesAsync();
+				}
+			}
 
-			//if (context.ProductCategories.Count() == 0)
-			//{
-			//	var categoriesdata = File.ReadAllText("../Talabat_Repository/Data/DataSeed/categories.json");
+			if (context.ProductCategories.Count() == 0)
+			{
+				var categoriesdata = File.ReadAllText("../Talabat_Repository/Data/DataSeed/categories.json");
 
-			//	var categories = JsonSerializer.Deserialize<List<ProductCategory>>(categoriesdata);
+				var categories = JsonSerializer.Deserialize<List<ProductCategory>>(categoriesdata);
 
-			//	if (categories?.Count > 0)
-			//	{
-			//		categories = categories.Select(b => new ProductCategory()
-			//		{
-			//			Name = b.Name,
-			//		}).ToList();
+				if (categories?.Count > 0)
+				{
+					categories = categories.Select(b => new ProductCategory()
+					{
+						Name = b.Name,
+					}).ToList();
 
-			//		foreach (var category in categories)
-			//		{
-			//			context.Set<ProductCategory>().Add(category);
-			//		}
+					foreach (var category in categories)
+					{
+						context.Set<ProductCategory>().Add(category);
+					}
 
-			//		await context.SaveChangesAsync();
-			//	}
-			//}
+					await context.SaveChangesAsync();
+				}
+			}
 
 			if (context.Products.Count() == 0)
 			{
