e05721e56d3e6cf41c564902456c94e0c6c9e03e
mahmoudaldaly <mahmo@DESKTOP-PVTMUIB>
2024-04-18 05:03:35 +0200

    Part 08 Error Types at API

---
+++ Talabat_APIs/Controllers/BuggyController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Talabat.Repository.Data;
+
+namespace Talabat.APIs.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class BuggyController : BaseAPIsController
+	{
+		private readonly StoreContext DbContext;
+
+		public BuggyController(StoreContext dbcontext)
+        {
+			DbContext = dbcontext;
+		}
+
+		[HttpGet("notfound")]
+		public ActionResult GetNotFoundRequest()
+		{
+			var product = DbContext.Products.Find(100);
+
+			if (product is null)
+			{
+				return NotFound();
+			}
+
+			return Ok(product);
+		}
+
+		[HttpGet("servererror")]
+		public ActionResult GetNotFoundResponse()
+		{
+			var product = DbContext.Products.Find(100);
+
+			var producttoreturn = product.ToString();
+
+			return Ok(producttoreturn);
+		}
+
+		[HttpGet("badrequest")]
+		public ActionResult GetBadRequest()
+		{
+			return BadRequest();
+		}
+
+		[HttpGet("badrequest/{id}")]
+		public ActionResult GetBadRequest(int id)
+		{
+			return Ok();
+		}
+
+	}
+}
